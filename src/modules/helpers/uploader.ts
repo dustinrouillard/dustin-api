085d0a8a698719a5fbd128d09e856b0e3d88dab0
Dustin Rouillard <me@dustin.sh>
2021-03-08 14:43:44 -0700

    Custom type stuff for text/plain raw text.

--- src/modules/helpers/uploader.ts
+++ src/modules/helpers/uploader.ts
@@ -6,20 +6,43 @@ import { MinioStorage } from 'modules/config';
 
 import { Multipart } from 'fastify-multipart';
 
+const mimeConversations: { [key: string]: string } = {
+  'text/plain': '.txt'
+};
+
+interface FileType {
+  mime: string;
+  extension: string;
+}
+
+async function GetType(file: Multipart, buffer: Buffer): Promise<FileType> {
+  const preType = file.mimetype;
+
+  let bufferType;
+  if (Object.keys(mimeConversations).includes(preType)) {
+    bufferType = { mime: preType, ext: mimeConversations[preType] };
+  } else bufferType = await typeFromBuffer(buffer);
+
+  return {
+    mime: bufferType?.mime || preType,
+    extension: bufferType?.ext || mimeConversations[preType]
+  };
+}
+
 export async function Upload(file: Multipart, type: UploadType, name?: string): Promise<string> {
   // Get buffer and file type
   const buffer = await file.toBuffer();
-  const file_type = await typeFromBuffer(buffer);
+  const file_type = await GetType(file, buffer);
 
   // Ignore mimetypes that are not allowed
-  if (!file_type?.mime || !(type == 'file' ? AlllowedFileTypes : AlllowedTypes).includes(file_type?.mime)) throw { code: 'invalid_file_type', data: file_type?.mime };
+  if (!file.mimetype || !(type == 'file' ? AlllowedFileTypes : AlllowedTypes).includes(file.mimetype)) throw { code: 'invalid_file_type', data: file.mimetype };
 
   // Create the sha1 hash for the file name
   const hash = createHash('sha1').update(buffer).digest('hex').substring(0, 16);
 
   // Folder and file path
   const folder = type == 'image' ? MinioStorage.ImagesFolder : MinioStorage.UploadsFolder;
-  const file_path = `${name || hash}.${file_type.ext}`;
+  const file_path = `${name || hash}.${file_type.extension}`;
 
   // Upload file using the upload utility
   await UploadFile({
