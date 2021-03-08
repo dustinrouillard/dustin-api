40ec80ab44752a1110a7fba40f23eaeb37afd4f0
Dustin Rouillard <me@dustin.sh>
2021-03-08 14:50:34 -0700

    Add failure for no file

--- src/modules/handlers/upload.ts
+++ src/modules/handlers/upload.ts
@@ -1,12 +1,15 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
 
-import { Success, Catch } from '@dustinrouillard/fastify-utilities/modules/response';
+import { Success, Catch, Failed } from '@dustinrouillard/fastify-utilities/modules/response';
 import { Debug } from '@dustinrouillard/fastify-utilities/modules/logger';
 
 import { Upload } from 'helpers/uploader';
 
 export async function UploadImageHandler(req: FastifyRequest<{ Body: { file: string; name?: string } }>, reply: FastifyReply): Promise<void> {
   try {
+    const file = await req.file();
+    if (!file) return Failed(reply, 400, 'missing_file');
+
     const URL = await Upload(await req.file(), 'image', req.body ? req.body.name : undefined);
 
     return Success(reply, 200, URL);
@@ -18,6 +21,9 @@ export async function UploadImageHandler(req: FastifyRequest<{ Body: { file: str
 
 export async function UploadFileHandler(req: FastifyRequest<{ Body: { file: string; name?: string } }>, reply: FastifyReply): Promise<void> {
   try {
+    const file = await req.file();
+    if (!file) return Failed(reply, 400, 'missing_file');
+
     const URL = await Upload(await req.file(), 'file', req.body ? req.body.name : undefined);
 
     return Success(reply, 200, URL);
