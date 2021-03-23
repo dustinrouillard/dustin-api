3f95a883ddd8d32d133c2e9f44caf15d062a6267
Dustin Rouillard <me@dustin.sh>
2021-03-22 19:02:54 -0600

    Add module disabling for minio

--- src/modules/utils/minio.ts
+++ src/modules/utils/minio.ts
@@ -1,14 +1,17 @@
 import { Client } from 'minio';
 
-import { MinioStorage } from 'modules/config';
+import { MinioStorage, ModulesDisabled } from 'modules/config';
 
-const minio = new Client({
-  endPoint: MinioStorage.Config.endpoint,
-  useSSL: MinioStorage.Config.ssl,
-  port: MinioStorage.Config.port,
-  accessKey: MinioStorage.Config.accessKey,
-  secretKey: MinioStorage.Config.secretKey
-});
+let minio: Client;
+
+if (!ModulesDisabled.FILES)
+  minio = new Client({
+    endPoint: MinioStorage.Config.endpoint,
+    useSSL: MinioStorage.Config.ssl,
+    port: MinioStorage.Config.port,
+    accessKey: MinioStorage.Config.accessKey,
+    secretKey: MinioStorage.Config.secretKey
+  });
 
 export type UploadType = 'image' | 'file';
 export const AlllowedTypes = ['image/gif', 'image/png', 'image/svg', 'image/webp', 'image/jpeg'];
@@ -51,6 +54,8 @@ interface UploadOptions {
 
 export async function UploadFile(options: UploadOptions): Promise<boolean> {
   return new Promise((resolve, reject) => {
+    if (!minio) return reject('not_enabled');
+
     // Metadata and content dispostion
     const metadata: { [key: string]: string | object } = { 'content-type': options.type };
     if (!(options.type.startsWith('image/') && !options.type.includes('svg')) && !options.type.startsWith('video/') && !options.type.startsWith('text/')) metadata.contentDisposition = 'attachment';
