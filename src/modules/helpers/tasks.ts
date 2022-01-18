c65d600b6c0047a17510b286e2ac9ec9f4e1a4ef
Dustin Rouillard <code@dstn.to>
2022-01-18 14:15:15 -0700

    Removing task for github readme

--- src/modules/helpers/tasks.ts
+++ src/modules/helpers/tasks.ts
@@ -1,11 +1,9 @@
-import { UpdateGitHubReadme } from 'modules/tasks/update_readme';
 import { GetDevelopmentHours } from 'modules/tasks/development_hours';
 import { PullTwitterFollowers } from 'modules/tasks/index_twitter_followers';
 import { UpdateStatisticsGist } from 'modules/tasks/update_gist';
 import { LogSpotifyListenHistory } from 'modules/tasks/spotify_listening_history';
 
 const tasks: { [key: string]: () => Promise<void> } = {
-  github_readme: UpdateGitHubReadme,
   development_hours: GetDevelopmentHours,
   twitter_followers: PullTwitterFollowers,
   github_gist: UpdateStatisticsGist,
